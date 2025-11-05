18a88bcb0c7ad93392c271d6f55d4b95b3884aed
Juan Camilo Ramón Pérez <jcramonp@eafit.edu.co>
2025-11-04 20:39:45 -0500

    Creacion de admin movidel

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/env bash
+
+
+export DJANGO_SETTINGS_MODULE=movidle.settings
+
 set -e
 set -x  # <- más logs
 
@@ -13,21 +16,26 @@ python manage.py collectstatic --noinput
 # Crear/actualizar superusuario (idempotente)
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
 python - <<'PY'
+import os, django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "movidle.settings")
+django.setup()
+
 from django.contrib.auth import get_user_model
-import os
 User = get_user_model()
+
 username = os.environ["DJANGO_SUPERUSER_USERNAME"]
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL","")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
 password = os.environ["DJANGO_SUPERUSER_PASSWORD"]
 
 u, created = User.objects.get_or_create(username=username, defaults={"email": email})
 u.is_superuser = True
 u.is_staff = True
-u.set_password(password)  # <-- fuerza/actualiza la contraseña
+u.set_password(password)   # fuerza/actualiza la contraseña
 u.save()
 print("Superuser", "created" if created else "updated")
 PY
 fi
+
 
 # Logs a stdout/stderr y puerto 8000
 exec gunicorn movidle.wsgi:application \
